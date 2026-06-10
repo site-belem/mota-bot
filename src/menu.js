984925999efea4181307ae58ac85e3b2f4423240
site-belem <site-belem@users.noreply.github.com>
2026-06-10 19:33:30 -0300

    Docs: update menu with .teste command

--- src/menu.js
+++ src/menu.js
@@ -4,63 +4,66 @@ const { getGreeting, getTime } = require('./utils');
 const { config } = require('./config');
 
 module.exports = async (sock, from, pushName, prefix) => {
-    const menuText = `✨ *𝐎𝐥𝐚́, ${pushName}!* ✨
-👋 *${getGreeting()}*
-🕒 *𝐇𝐨𝐫𝐚́𝐫𝐢𝐨:* ${getTime()}
-👑 *𝐃𝐎𝐍𝐎:* Mota
-💠 *𝐏𝐫𝐞𝐟𝐢𝐱𝐨𝐬:* [ ${config.prefixes.join(' ')} ]
+    const menuText = `✨ *𝐎𝐥𝐚́, ${pushName}!* ✨\n` +
+                     `👋 *${getGreeting()}*\n🕒 *𝐇𝐨𝐫𝐚́𝐫𝐢𝐨:* ${getTime()}\n👑 *𝐃𝐨𝐧𝐨:* ${config.ownerName}\n` +
+                     `💠 *𝐏𝐫𝐞𝐟𝐢𝐱𝐨𝐬:* [ ${config.prefixes.join(' ')} ]\n\n` +
+                     `🛠️ *𝐅𝐈𝐆𝐔𝐑𝐈𝐍𝐇𝐀𝐒*\n` +
+                     `┌ ♠️ *${prefix}s*\n` +
+                     `├ ♠️ *${prefix}sc*\n` +
+                     `├ ♠️ *${prefix}cr*\n` +
+                     `├ ♠️ *${prefix}cr2*\n` +
+                     `├ ♠️ *${prefix}rename*\n` +
+                     `└ ♠️ *${prefix}att*\n\n` +
+                                          `💵 *𝐀𝐋𝐔𝐆𝐔𝐄𝐋*
+` +
+                     `┌ ♠️ *${prefix}aluguel*
+` +
+                     `└ ♠️ *${prefix}alugar*
 
-💵 *𝐀𝐋𝐔𝐆𝐔𝐄𝐋*
-┌ ♠️ *${prefix}aluguel*
-├ ♠️ *${prefix}alugar*
-└ ♠️ *${prefix}infoteste*
-
-🌍 *𝐈𝐍𝐓𝐄𝐑𝐍𝐄𝐓*
-┌ ♠️ *${prefix}play*
-├ ♠️ *${prefix}baixar*
-├ ♠️ *${prefix}gpt*
-├ ♠️ *${prefix}img*
-├ ♠️ *${prefix}encurtar*
-├ ♠️ *${prefix}tr*
-├ ♠️ *${prefix}tst*
-└ ♠️ *${prefix}curiosidade*
-
-🎮 *𝐉𝐎𝐆𝐎𝐒*
-┌ ♠️ *${prefix}velha*
-├ ♠️ *${prefix}gado*
-└ ♠️ *${prefix}casal*
-
-⚙️ *𝐆𝐑𝐔𝐏𝐎*
-┌ ♠️ *${prefix}bemvindoon*
-├ ♠️ *${prefix}bemvindooff*
-├ ♠️ *${prefix}add*
-├ ♠️ *${prefix}ban*
-├ ♠️ *${prefix}seradm*
-├ ♠️ *${prefix}tiraradm*
-├ ♠️ *${prefix}mute*
-├ ♠️ *${prefix}unmute*
-├ ♠️ *${prefix}fechargp*
-├ ♠️ *${prefix}abrirgp*
-├ ♠️ *${prefix}limpar*
-└ ♠️ *${prefix}all*
-
-🔑 *𝐒𝐈𝐒𝐓𝐄𝐌𝐀*
-┌ ♠️ *${prefix}boton*
-├ ♠️ *${prefix}botoff*
-├ ♠️ *${prefix}addprefixo*
-├ ♠️ *${prefix}remprefixo*
-└ ♠️ *${prefix}infocmd*
-
-👑 *𝐃𝐎𝐍𝐎*
-┌ ♠️ *${prefix}addaluguel*
-├ ♠️ *${prefix}addteste*
-├ ♠️ *${prefix}remaluguel*
-└ ♠️ *${prefix}id*`;
-
+                     `🌍 *𝐈𝐍𝐓𝐄𝐑𝐍𝐄𝐓*\n` +
+                     `┌ ♠️ *${prefix}play*\n` +
+                     `├ ♠️ *${prefix}baixar*\n` +
+                     `├ ♠️ *${prefix}gpt*\n` +
+                     `├ ♠️ *${prefix}img*\n` +
+                     `├ ♠️ *${prefix}encurtar*\n` +
+                     `├ ♠️ *${prefix}tr*\n` +
+                     `├ ♠️ *${prefix}tst*\n` +
+                     `└ ♠️ *${prefix}curiosidade*\n\n` +
+                     `🎮 *𝐉𝐎𝐆𝐎𝐒*\n` +
+                     `┌ ♠️ *${prefix}velha*\n` +
+                     `├ ♠️ *${prefix}gado*\n` +
+                     `└ ♠️ *${prefix}casal*\n\n` +
+                     `⚙️ *𝐆𝐑𝐔𝐏𝐎*\n` +
+                     `┌ ♠️ *${prefix}bemvindoon*\n` +
+                     `├ ♠️ *${prefix}bemvindooff*\n` +
+                     `├ ♠️ *${prefix}add*\n` +
+                     `├ ♠️ *${prefix}ban*\n` +
+                     `├ ♠️ *${prefix}seradm*\n` +
+                     `├ ♠️ *${prefix}tiraradm*\n` +
+                     `├ ♠️ *${prefix}mute*\n` +
+                     `├ ♠️ *${prefix}unmute*\n` +
+                     `├ ♠️ *${prefix}fechargp*\n` +
+                     `├ ♠️ *${prefix}abrirgp*\n` +
+                     `├ ♠️ *${prefix}limpar*\n` +
+                     `└ ♠️ *${prefix}all*\n\n` +
+                     `🔑 *𝐒𝐈𝐒𝐓𝐄𝐌𝐀*\n` +
+                     `┌ ♠️ *${prefix}boton*\n` +
+                     `├ ♠️ *${prefix}botoff*\n` +
+                     `├ ♠️ *${prefix}addprefixo*\n` +
+                     `├ ♠️ *${prefix}remprefixo*\n` +
+                     `└ ♠️ *${prefix}infocmd*`\n\n" +
+`👑 *𝐃𝐎𝐍𝐎*
+` +
+                     `┌ ♠️ *${prefix}addaluguel*
+` +
+                     `├ ♠️ *${prefix}addteste*
+` +
+                     `├ ♠️ *${prefix}remaluguel*
+` +
+                     `└ ♠️ *${prefix}i;
+    
     const menuVideoPath = path.join(__dirname, '../assets/menu.mp4');
     if (fs.existsSync(menuVideoPath)) {
         await sock.sendMessage(from, { video: fs.readFileSync(menuVideoPath), caption: menuText, gifPlayback: true });
-    } else { 
-        await sock.sendMessage(from, { text: menuText }); 
-    }
+    } else { await sock.sendMessage(from, { text: menuText }); }
 };
